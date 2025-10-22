7f7de79be0b9cf026c19b03ed639b20e71fd1217
John French <john.french@atu.ie>
2025-10-22 23:44:23 +0100

    Config

--- src/main/java/ie/atu/cloudnative/catalogservice/controller/HomeController.java
+++ src/main/java/ie/atu/cloudnative/catalogservice/controller/HomeController.java
@@ -1,17 +1,25 @@
 package ie.atu.cloudnative.catalogservice.controller;
 
+import ie.atu.cloudnative.catalogservice.config.AtuProperties;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
 @RestController
 public class HomeController {
 
+    private final AtuProperties atuProperties;
+
+    public HomeController(AtuProperties atuProperties) {
+        this.atuProperties = atuProperties;
+    }
+
     @GetMapping("/")
     public String getGreeting() {
-        return "Welcome to the Cloud Native Book Catalog!";
+        return atuProperties.getGreeting();
     }
 
     @GetMapping("/greeting/{name}")
@@ -23,6 +31,6 @@ public class HomeController {
     public String health() throws UnknownHostException {
         String hostname = InetAddress.getLocalHost().getHostName();
         return "Application is healthy and running on: " + hostname +
-               " (" + System.getProperty("os.name") + ")";
+                " (" + System.getProperty("os.name") + ")";
     }
 }
